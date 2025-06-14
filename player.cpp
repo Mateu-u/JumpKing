0b658b4f95ff90c61ae44c9c741c64b21f4592c3
Mateu-u <129677176+Mateu-u@users.noreply.github.com>
2025-06-14 20:57:02 +0200

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -12,12 +12,13 @@ Player::Player(float x, float y,
                const std::map<std::string, sf::Texture>& textures,
                float scale)
     : texMap(textures)
+    , currentLevel(0)
     , velocity(0.f, 0.f)
-    , isOnGround(false)
     , jumpCharge(0.f)
     , chargingJump(false)
     , facingRight(true)
     , lastLandedPlat(-1)
+    , isOnGround(false)
 {
     setState("idle");
     sprite.setOrigin(
@@ -75,39 +76,58 @@ void Player::applyVelocityX() {
 }
 
 int Player::checkCollisions(const sf::FloatRect& prevBounds,
-                            const sf::FloatRect& currBounds,
-                            std::vector<sf::RectangleShape>& platforms)
+                          const sf::FloatRect& currBounds,
+                          std::vector<sf::RectangleShape>& platforms)
 {
     isOnGround = false;
     int landedIndex = -1;
 
     for (int i = 0; i < (int)platforms.size(); ++i) {
         const auto& plat = platforms[i];
-        sf::FloatRect tb = plat.getGlobalBounds();
+        sf::FloatRect platformBounds = plat.getGlobalBounds();
 
-        if (currBounds.intersects(tb)) {
-            float prevBottom = prevBounds.top + prevBounds.height;
-            float currBottom = currBounds.top + currBounds.height;
-            float prevTop    = prevBounds.top;
-            float currTop    = currBounds.top;
+        if (currBounds.intersects(platformBounds)) {
+            // Obliczamy wszystkie potrzebne pozycje
+            float playerBottom = currBounds.top + currBounds.height;
+            float playerTop = currBounds.top;
+            float playerLeft = currBounds.left;
+            float playerRight = currBounds.left + currBounds.width;
 
-            // lądowanie
-            if (prevBottom <= tb.top && velocity.y >= 0) {
-                sprite.setPosition(
-                    sprite.getPosition().x,
-                    tb.top - currBounds.height / 2.f
-                    );
-                velocity.y   = 0.f;
-                isOnGround   = true;
-                landedIndex  = i;
+            float platformTop = platformBounds.top;
+            float platformBottom = platformBounds.top + platformBounds.height;
+            float platformLeft = platformBounds.left;
+            float platformRight = platformBounds.left + platformBounds.width;
+
+            // Obliczamy głębokości penetracji z każdej strony
+            float overlapLeft = playerRight - platformLeft;
+            float overlapRight = platformRight - playerLeft;
+            float overlapTop = playerBottom - platformTop;
+            float overlapBottom = platformBottom - playerTop;
+
+            // Znajdujemy najmniejszą penetrację
+            float minOverlap = std::min({overlapLeft, overlapRight, overlapTop, overlapBottom});
+
+            // Kolizja od góry (lądowanie)
+            if (minOverlap == overlapTop && velocity.y >= 0) {
+                sprite.setPosition(sprite.getPosition().x, platformTop - currBounds.height/2.0f);
+                velocity.y = 0.0f;
+                isOnGround = true;
+                landedIndex = i;
             }
-            // uderzenie głową
-            else if (prevTop >= tb.top + tb.height && velocity.y < 0) {
-                sprite.setPosition(
-                    sprite.getPosition().x,
-                    tb.top + tb.height + currBounds.height / 2.f
-                    );
-                velocity.y = 0.f;
+            // Kolizja od dołu (głową w sufit)
+            else if (minOverlap == overlapBottom && velocity.y < 0) {
+                sprite.setPosition(sprite.getPosition().x, platformBottom + currBounds.height/2.0f);
+                velocity.y = 0.0f;
+            }
+            // Kolizja z lewej strony
+            else if (minOverlap == overlapLeft) {
+                sprite.setPosition(platformLeft - currBounds.width/2.0f, sprite.getPosition().y);
+                velocity.x = 0.0f;
+            }
+            // Kolizja z prawej strony
+            else if (minOverlap == overlapRight) {
+                sprite.setPosition(platformRight + currBounds.width/2.0f, sprite.getPosition().y);
+                velocity.x = 0.0f;
             }
         }
     }
